03862a6990db752c3edcbe532b6dcb66ee671d44
JavierPM95 <javierpm1995@gmail.com>
2020-08-18 05:13:57 -0300

    logo animated

---
+++ css/main.js
@@ -0,0 +1,7 @@
+console.log("HOLA")
+
+/*const logo = document.querySelectorAll("#logo-svg path");
+
+for (let i = 0; i < logo.length; i++) {
+    console.log("Letter ${i} is ${logo[i].getTotalLeght()}");
+}
