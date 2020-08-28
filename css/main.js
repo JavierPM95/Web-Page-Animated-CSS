ba17eba5524c174eb1802b4dbd02b7bd21b1b244
JavierPM95 <javierpm1995@gmail.com>
2020-08-28 19:28:59 -0300

    Form-Animation

--- css/main.js
+++ css/main.js
@@ -1,7 +1,22 @@
-console.log("HOLA")
+var inputText = document.getElementsByClassName('input-text');
+var inputMessage = document.getElementsByClassName('input-message');
 
-/*const logo = document.querySelectorAll("#logo-svg path");
+for (var i = 0; i < inputText.length; i++) {
+    inputText[i].addEventListener('keyup', function(){
+        if(this.value.length>=1) {
+            this.nextElementSibling.classList.add('set-text');
+        } else {
+            this.nextElementSibling.classList.remove('set-text');
+        }
+    });
+}
 
-for (let i = 0; i < logo.length; i++) {
-    console.log("Letter ${i} is ${logo[i].getTotalLeght()}");
+for (var i = 0; i < inputMessage.length; i++) {
+    inputMessage[i].addEventListener('keyup', function(){
+        if(this.value.length>=1) {
+            this.nextElementSibling.classList.add('set-message');
+        } else {
+            this.nextElementSibling.classList.remove('set-message');
+        }
+    });
 }
